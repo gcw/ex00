dce72a74346dbf02f47400f161da3c155c0dd06d
G. Clifford Williams <gcw-github@notadiscussion.com>
2019-03-30 13:51:58 -0500

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2.7
+import sys 
+
+like={}
+dislike={}
+for line in sys.stdin:
+    if line.startswith('|'):
+        like[count], dislike[count] = [ x.rstrip() for x in line.split('|')[1:3] ] 
+    else:
+        print( "like: %s" % " ".join([ x.lstrip() for x in like.values()]) )
+        print( "dislike: %s" % \n" ".join([ x.lstrip() for x in dislike.values()]) )
+        like = {}
+        dislike = {}
