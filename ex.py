46f1c88dac6dcb8587b0e2ecf713c54cf31d3531
G. Clifford Williams <gcw-github@notadiscussion.com>
2019-03-31 14:16:04 -0500

    Update ex.py

--- ex.py
+++ ex.py
@@ -8,6 +8,6 @@ for line in sys.stdin:
         like[count], dislike[count] = [ x.rstrip() for x in line.split('|')[1:3] ] 
     else:
         print( "like: %s" % " ".join([ x.lstrip() for x in like.values()]) )
-        print( "dislike: %s" % \n" ".join([ x.lstrip() for x in dislike.values()]) )
+        print( "dislike: %s\n" % " ".join([ x.lstrip() for x in dislike.values()]) )
         like = {}
         dislike = {}
